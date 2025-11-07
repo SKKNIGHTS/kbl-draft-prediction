893a95114679169702145a9ac65bbcb923c6b162
권수연 <kwonsuyeon@gwonsuyeon-ui-MacBookAir.local>
2025-11-07 23:17:08 +0900

    백엔드 업데이트

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",  // ✅ 반드시 FastAPI 주소
+  baseURL: "https://kbl-draft-prediction.onrender.com",  
 });
 
 export default api;
